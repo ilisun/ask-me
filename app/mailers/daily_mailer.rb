8951eac89e6a61893f6bc0498c516081016e9474
ilisun <solaril@mail.ru>
2016-01-27 13:11:19 +0300

    change mailer

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -3,7 +3,6 @@ class DailyMailer < ApplicationMailer
   def digest(user)
     @user = user
     @url  = 'http://ask-me.borovinskiy.com'
-    mail from: "ask-digest@ask-me.borovinskiy.com"
     mail to: user.email
   end
 end
