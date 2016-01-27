64ec10c27427ad54ab79e16e2157a5530f33fa3e
ilisun <solaril@mail.ru>
2016-01-27 10:05:25 +0300

    comment sort default

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-  
-  default_scope { order(created_at: :desc) }
+
+  default_scope { order(created_at: :asc) }
 
   belongs_to :user
   belongs_to :commentable, polymorphic: true
